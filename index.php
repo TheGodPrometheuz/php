d9d8f0f1f901abc114a5908e40a344cbb5f06264
TheGodPrometheuz <79011835+TheGodPrometheuz@users.noreply.github.com>
2021-02-15 10:13:50 -0300

    oposto

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <<<<<<< HEAD
 <?php
 
-	echo "Codigos choram sim";
+	echo "Codigos não choram";
 
 ?>
-=======
-<?php
-
-	echo "Codigos não choram sim";
-
-?>
->>>>>>> 3e0c670bd8088aad095c2ac14f1266daf7ead101
